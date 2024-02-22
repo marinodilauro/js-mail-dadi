36e14d82465d26b25b183801ac4034c04f88b189
marinodilauro <m.dilauro90@gmail.com>
2024-02-22 15:34:51 +0100

    Create variables for email section

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// Creo una lista con le email che hanno accesso
+
+const emailList = [
+  "marino@yahoo.it",
+  "jimmyo@hotmail.com",
+  "andrea@outlook.it",
+  "simona@virgilio.it",
+  "chiara@virgilio.it",
+  "piera@alice.it",
+  "lello@gmail.com"
+];
+
+
