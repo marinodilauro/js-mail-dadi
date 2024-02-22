50c7384cc6fd3c0c8d2f040a8baaebdf014d3635
marinodilauro <m.dilauro90@gmail.com>
2024-02-22 16:21:57 +0100

    Add layout and style for login section

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,9 @@ const emailList = [
 
 // Creo una variabile con l'email dell'utente
 
-const userEmail = "mario@sitostrano.it";
+const inputEmailElement = document.getElementById("inputEmail");
+
+let userEmail = inputEmailElement.value;
 
 
 // Creo una variabile booleana per verificare se l'utente ha accesso
@@ -23,24 +25,46 @@ const userEmail = "mario@sitostrano.it";
 let hasAccess = false;
 
 
-// Creo un ciclo for per controllare i dati all'interno della lista
+const submitButtonElement = document.getElementById("submitBtn");
 
-for (let i = 0; i < emailList.length; i++) {
-  const email = emailList[i];
+submitButtonElement.addEventListener("click", function (e) {
 
-  if (userEmail === email) {
-    hasAccess = true;
-  }
+  e.preventDefault();
 
-};
+  userEmail = inputEmailElement.value;
 
-// Stampa il risultato in console
+  // Creo un ciclo for per controllare i dati all'interno della lista
 
-if (hasAccess === true) {
-  console.log(`Ciao ${userEmail.split("@")[0]}, bentornato/a!`)
-} else {
-  console.log(`Spiacente ${userEmail.split("@")[0]}, la tua email non è presente in archivio!`)
-};
+  for (let i = 0; i < emailList.length; i++) {
+    const email = emailList[i];
+
+    if (userEmail === email) {
+      hasAccess = true;
+    }
+
+  };
+
+  // Stampa il risultato in console
+
+  const loginOutputElement = document.querySelector(".mc_login_output");
+
+  if (hasAccess === true) {
+
+    //console.log(`Ciao ${userEmail.split("@")[0]}, bentornato/a!`)
+    loginOutputElement.innerHTML = `<div>Ciao ${userEmail.split("@")[0]}, bentornato/a!</div>`;
+
+  } else {
+
+    // console.log(`Spiacente ${userEmail.split("@")[0]}, la tua email non è presente in archivio!`)
+    loginOutputElement.innerHTML = `Spiacente ${userEmail.split("@")[0]}, la tua email non è presente in archivio!`;
+
+  };
+
+  hasAccess = false;
+
+});
+
+
 
 ////////// #endregion USER ACCESS //////////
 
@@ -55,7 +79,7 @@ const cpuNumber = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
 console.log(userNumber, cpuNumber);
 
 
-// Verifico quale dei due valori è maggiore
+// Verifico quale dei due valori è maggiore e stampo il risultato
 
 if (userNumber > cpuNumber) {
   console.log("You win!");
